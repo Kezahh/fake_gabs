65f84bc79951f4b6e7166a68b0de1c97ec9a27b7
Kerryn <example@example.com>
2025-05-24 10:34:44 +1000

    add web build files

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4189642743"
+    serviceWorkerVersion: "2619066755"
   }
 });
